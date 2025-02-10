e0c2a8e0eeeb46ac37a3c2bbedc25262a93182a6
cal-dortiz <djortiz@berkeley.edu>
2025-02-09 21:30:53 -0600

    Add grade node:

---
+++ advanced_rag_flow/graph/chains/tests/test_chains.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from ingestion import retriever
+
+load_dotenv()
+
+# The trick here is to make the question and doc relative to eachother to force a yes
+def test_retrieval_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+
+    assert res.binary_score == "yes"
+
+def test_retrieval_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": "how to make pizza", "document": doc_txt}
+    )
+
+    assert res.binary_score == "no"
